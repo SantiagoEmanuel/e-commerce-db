0362af97f73a8d4068453ac560c21ab075641a70
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-04-23 13:20:37 -0300

    add cartRouter and remove query from express

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express, { json, query } from 'express';
+import express, { json } from 'express';
 import { corsMiddleware } from './middleware/corsMiddleware.js';
 import { disable } from './middleware/disable.js';
 import { productRouter } from './routes/products.js';
 import { userRouter } from './routes/users.js'
+import { cartRouter } from './routes/carts.js';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(json({ "content-type": "application/json" }))
 app.use(corsMiddleware());
@@ -17,10 +18,11 @@ app.use('/products', productRouter)
 // USER ROUTES.
 app.use('/user', userRouter)
 
+// CART ROUTES
+app.use('/cart', cartRouter)
+
 app.use((req, res) => {
      res.status(404).json({ status: 404, message: 'Not Found' })
 })
 
-app.listen(PORT, () => {
-     console.log(PORT)
-})
+app.listen(PORT)
