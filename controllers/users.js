d9425892176a62bd27df6e21d966240671474a02
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-04-23 13:27:57 -0300

    add token response an generate to user login

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,31 @@
 import { UserModel } from "../models/user.js";
 import { validateUser } from "../scheme/user.js";
+import jwt from 'jsonwebtoken'
 
 export class UserController {
      static async getUser(req, res) {
+          const secret = process.env.SECRET_KEY
+
           const { username, password } = req.query;
 
           const result = await UserModel.getUser({ username: username, password: password });
 
           if (result.length == 0) { return res.json({ message: 'user not found' }).status(400) }
 
-          res.json({ userInfo: result }).status(200)
+          const token = jwt.sign({
+               "id": result.id,
+               "username": result.username,
+               "email": result.email,
+               "first_name": result.first_name,
+               "last_name": result.last_name,
+               "status": result.status
+          },
+               secret,
+               {
+                    expiresIn: '1h',
+               })
+
+          res.json({ userInfo: result[0], token: token }).status(200)
      }
 
      static async createUser(req, res) {
@@ -21,7 +37,7 @@ export class UserController {
           const data = await UserModel.createUser(result.data)
 
           res.json({
-               'userInfo': data
+               'userInfo': data[0]
           }).status(201);
      }
 }
