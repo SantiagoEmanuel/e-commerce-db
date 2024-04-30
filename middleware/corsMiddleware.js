00c61d0b1e3688f0f20c61dc7a9a12467c234b92
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-04-30 20:46:48 -0300

    add more host allowed

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,6 +1,14 @@
 import cors from 'cors';
 
-const ALLOW_HOST = process.env.ALLOW_HOST
+const ALLOW_HOST = process.env.ALLOW_HOST || [
+     'https://e-commerce-delta-livid-65.vercel.app/',
+     'http://localhost:5173',
+     'http://localhost:5173/futbol',
+     'http://localhost:5173/voley',
+     'http://localhost:5173/bastquet',
+     'http://localhost:5173/login',
+     'http://localhost:5173/create-product'
+];
 
 export const corsMiddleware = ({ acceptedOrigins = ALLOW_HOST } = {}) => cors({
      origin: (origin, callback) => {
