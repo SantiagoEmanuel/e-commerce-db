6ae66225db83287882f95f82ae4ecaac9c929960
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-04-30 19:25:23 -0300

    remove cors middleware

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express, { json } from 'express';
-import { corsMiddleware } from './middleware/corsMiddleware.js';
+import cors from 'cors';
 import { disable } from './middleware/disable.js';
 import { productRouter } from './routes/products.js';
 import { userRouter } from './routes/users.js'
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(json())
-app.use(corsMiddleware());
+app.use(cors());
 app.disable(disable)
 
 // TODO ▶️ create "categories" route and create categories in database, create the reference with products.
