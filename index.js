af2e48463ae494c764ce8a90888e1b413e5539be
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-07 08:35:35 -0300

    remove empty lines

--- index.js
+++ index.js
@@ -5,28 +5,22 @@ import { productRouter } from './routes/products.js';
 import { userRouter } from './routes/users.js'
 import { cartRouter } from './routes/carts.js';
 import { categoriesRouter } from './routes/categories.js';
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app.use(json())
 app.use(corsMiddleware());
 app.disable(disable)
 
+
 // TODO ▶️ create "categories" route and create categories in database, create the reference with products.
 app.use('/categories', categoriesRouter)
-
 // PRODUCT ROUTES.
 app.use('/products', productRouter)
-
 // USER ROUTES.
 app.use('/user', userRouter)
-
 // CART ROUTES
 app.use('/cart', cartRouter)
-
 app.use((req, res) => {
      res.status(404).json({ status: 404, message: 'Not Found' })
 })
-
 app.listen(PORT)
