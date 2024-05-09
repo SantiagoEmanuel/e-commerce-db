e1ceee7e1ffde32265a9491dbe02ce846784249e
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-09 19:00:15 -0300

    Optimice code to create new user

--- controllers/users.js
+++ controllers/users.js
@@ -2,9 +2,9 @@ import { UserModel } from "../models/user.js";
 import { validateUser } from "../scheme/user.js";
 import jwt from 'jsonwebtoken'
 
+const secret = process.env.SECRET_KEY
 export class UserController {
      static async getUser(req, res) {
-          const secret = process.env.SECRET_KEY || "secretKey-ecommerce-authorization"
           const { username, password } = req.headers;
           const result = await UserModel.getUser({ username: username, password: password });
           if (result.length == 0) { return res.json({ message: 'user not found' }).status(400) }
@@ -23,14 +23,14 @@ export class UserController {
           return res.json({ userInfo: result[0], token: token }).status(200)
      }
 
-     static async createUser(req, res) {
+     static createUser(req, res) {
           const result = validateUser({ input: req.body })
           if (!result.success) {
                return res.json({ error: JSON.parse(result.error.message) }).status(400)
           } else {
-               const data = await UserModel.createUser(result.data)
+               UserModel.createUser(result.data)
                return res.json({
-                    'userInfo': data[0]
+                    ok: true
                }).status(201);
           }
      }
--- models/user.js
+++ models/user.js
@@ -9,17 +9,12 @@ export class UserModel {
           return rows;
      }
 
-     static async createUser({ username, email, first_name, last_name, password, status }) {
+     static createUser({ username, email, first_name, last_name, password, status }) {
           try {
                db.execute({
                     sql: 'insert into users(username, email, first_name, last_name, password, status) values (?,?,?,?,?,?)',
                     args: [username, email, first_name, last_name, password, status]
                })
-               const { rows } = await db.execute({
-                    sql: 'select username, password from users where username = ? and password = ?',
-                    args: [username, password]
-               })
-               return rows;
           } catch (error) {
                throw new Error({ error: error.message })
           }
