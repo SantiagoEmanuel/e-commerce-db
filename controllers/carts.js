571b13936618e8178cecc2a0385de57798367f6a
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-05-12 19:00:30 -0300

    delete products where that not exist in newCart and delete cart where cart is undefined

--- controllers/carts.js
+++ controllers/carts.js
@@ -10,15 +10,28 @@ export class CartController {
 
      static async addCart(req, res) {
           const { id_user, cart } = req.body;
-          const rows = await CartModel.getCart(id_user);
-          const newCart = Object.entries(cart);
-          newCart.map(([key, count]) => {
-               if (rows.some(({ id }) => id == key)) {
-                    CartModel.updateCart(count, key, id_user)
-               } else {
-                    CartModel.addCart(id_user, key, count)
-               }
-          })
+          if (cart) {
+               const rows = await CartModel.getCart(id_user);
+               const newCart = Object.entries(cart);
+               newCart.map(([key, count]) => {
+                    if (rows.some(({ id }) => id == key)) {
+                         CartModel.updateCart(count, key, id_user)
+                    } else {
+                         CartModel.addCart(id_user, key, count)
+                    }
+               })
+
+               rows.map(({ id }) => {
+                    if (!newCart.some(([key]) => key == id)) {
+                         CartModel.deleteItemCart(id)
+                    }
+               })
+          } else {
+               CartModel.deleteCart(id_user)
+               return res.json({
+                    cart: 'Cart delete'
+               }).status(200)
+          }
           return res.json({
                cart: 'Cart upgrade'
           }).status(200)
