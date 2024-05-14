c6df8e87283db462015d396a7becce11500e3ae1
Santiago Emanuel Mustafa Font <119715217+SantiagoEmanuel@users.noreply.github.com>
2024-05-13 22:51:21 -0300

    Remove allow_host array

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,14 +1,6 @@
 import cors from 'cors';
 
-const ALLOW_HOST = process.env.ALLOW_HOST || [
-     'https://e-commerce-delta-livid-65.vercel.app/',
-     'http://localhost:5173',
-     'http://localhost:5173/futbol',
-     'http://localhost:5173/voley',
-     'http://localhost:5173/bastquet',
-     'http://localhost:5173/login',
-     'http://localhost:5173/create-product'
-];
+const ALLOW_HOST = process.env.ALLOW_HOST
 
 export const corsMiddleware = ({ acceptedOrigins = ALLOW_HOST } = {}) => cors({
      origin: (origin, callback) => {
